eef45a0789df31795891073af44977d6314ef8d0
Mold Management <mold-management@example.com>
2026-07-05 21:24:29 +0800

    全屏max-height改为-26px,普通模式恢复-170px

--- src/views/ScrewSpec.vue
+++ src/views/ScrewSpec.vue
@@ -18,7 +18,7 @@
         </div>
       </template>
 
-        <el-table :data="tableData" border style="width: 100%" :max-height="isFullscreen ? 'calc(100vh - 10px)' : 'calc(100vh - 26px)'" v-loading="loading">
+        <el-table :data="tableData" border style="width: 100%" :max-height="isFullscreen ? 'calc(100vh - 26px)' : 'calc(100vh - 170px)'" v-loading="loading">
         <el-table-column prop="name" label="螺丝名称" width="160" sortable />
         <el-table-column prop="headType" label="头型" width="120" sortable :filters="headTypeFilters" :filter-method="filterHandler" />
         <el-table-column prop="punch" label="冲头" width="120" sortable>
